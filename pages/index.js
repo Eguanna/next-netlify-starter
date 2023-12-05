a1d514ffd8b3f48321783ea198535cc819a270ff
jihwan <80106655+Eguanna@users.noreply.github.com>
2023-12-06 02:28:59 +0900

    Update index.js
    내용점검1
    파일 업로딩 전

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,18 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  // Get started by editing <code>pages/index.js
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>창융1 시연</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          
         </p>
       </main>
 
